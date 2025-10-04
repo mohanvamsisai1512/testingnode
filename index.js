242cd1e0688cb3c800c54ac33e43fe297aa9e19f
mohan vamsi <mohanvamsisai1512@gmail.com>
2025-10-04 23:51:42 +0530

    initail commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import express
+const express = require("express");
+
+// Create an Express app
+const app = express();
+
+// Define a port
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON (optional)
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
+}); 
+
+app.get("/page2", (req, res) => {
+  res.send("Hello from Express server page2!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
