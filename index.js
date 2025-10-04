070898b3c6f3a1de5194247519ab6a1e59be32af
mohan vamsi <mohanvamsisai1512@gmail.com>
2025-10-05 00:06:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
 
 app.get("/page2", (req, res) => {
   res.send("Hello from Express server page2!");
+}); 
+
+app.get("/page3", (req, res) => {
+  res.send("Hello from Express server page3!");
 });
 
 // Start the server
